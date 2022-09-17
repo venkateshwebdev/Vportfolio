42e72e4e0f0e0e1aefde8e7efa8651a2faa4eda1
venkateshwebdev <105224564+venkateshwebdev@users.noreply.github.com>
2022-09-18 01:31:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// var a = document.getElementById("hangman");
-// var b = document.getElementById("rps");
-// var c = document.getElementById("hhhh")
-// a.addEventListener("mouseover",function(){
-//   b.style.opacity="0";
-//   c.style.opacity="0";
-// });
-// a.addEventListener("mouseout",function(){
-//   b.style.opacity="1";
-//   c.style.opacity="1";
-// });
-// b.addEventListener("mouseover",function(){
-//   a.style.opacity="0";
-//   c.style.opacity="0";
-// });
-// b.addEventListener("mouseout",function(){
-//   a.style.opacity="1";
-//   c.style.opacity="1";
-// });
-//
-// c.addEventListener("mouseover",function(){
-//   a.style.opacity="0";
-//   b.style.opacity="0";
-// });
-// c.addEventListener("mouseout",function(){
-//   a.style.opacity="1";
-//   b.style.opacity="1";
-// });
-
 
 var cards = [
   document.getElementById("hangman"),
